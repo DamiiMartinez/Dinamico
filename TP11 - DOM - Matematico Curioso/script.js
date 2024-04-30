e514ec3e9dc274cc40487fc2648f1273789661ec
DamiiMartinez <162657068+DamiiMartinez@users.noreply.github.com>
2024-04-30 13:51:11 -0300

    Delete TP11 - DOM - Matematico Curioso directory

--- TP11 - DOM - Matematico Curioso/script.js
+++
@@ -1,7 +0,0 @@
-function filtrarum(){
-    window.addEventListener('load', inicio, false);
-    document.getElementById('documento').addEventListener('change', cargar, false);
-    var archivo = new FileReader();
-    archivo.addEventListener('load', leer, false);
-    archivo.readAsText(ev)
-}
