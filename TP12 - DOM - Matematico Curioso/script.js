bb3be2867c2f1025b768deb5477ac05fae1539c1
DamiiMartinez <162657068+DamiiMartinez@users.noreply.github.com>
2024-04-30 14:08:44 -0300

    Update script.js

--- TP12 - DOM - Matematico Curioso/script.js
+++ TP12 - DOM - Matematico Curioso/script.js
@@ -6,17 +6,23 @@ function ingresarSemilla(){
     for(let i=0; i<array.length; i++){
         if( semilla == 1 ){ }
         else{
-            if( semilla%2 == 0 ){
-                numero = semilla/2;
-                let vector1 = array.push(numero);
-            }
-            if( semilla%2 != 0 ){
-                numero = semilla*3;
-                numero = numero+1;
-                let vector2 = array.push(numero);
-            }
+            semilla = verificar(semilla, array);
             semilla = array[i+1];
         }
     }
     alert(array);
+}
+
+function verificar( semilla, array ){
+    let numero=0;
+    if( semilla%2 == 0 ){
+        numero = semilla/2;
+        let vector1 = array.push(numero);
+    }
+    if( semilla%2 != 0 ){
+        numero = semilla*3;
+        numero = numero+1;
+        let vector2 = array.push(numero);
+    }
+    return semilla;
 }
